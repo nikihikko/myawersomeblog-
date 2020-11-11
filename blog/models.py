a7ee5e7bb6cbb61c2316754af9dde6b432b9a768
hikka <kolya.dubina99@gmail.com>
2020-11-11 21:10:23 +0300

    test commit

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=300)
+    date = models.DateTimeField()
+    text = models.TextField()
+    event_image = models.ImageField(upload_to='event_image/')
